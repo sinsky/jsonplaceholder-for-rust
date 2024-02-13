ab74d57401aa2ba6d27b19766030ea865f2d1d89
sinsky <60662802+sinsky@users.noreply.github.com>
2024-02-14 01:02:16 +0900

    doc: generate documentation

--- src/posts/api.rs
+++ src/posts/api.rs
@@ -1,23 +1,83 @@
 use serde::Deserialize;
 
+/// jsonplaceholderのpost構造
 #[derive(Debug, Deserialize)]
 
 pub struct Post {
+    /// * `u32` - Post項目のID
     #[serde(rename = "userId")]
     pub user_id: u32,
+    /// * `u32` - Post項目のID
     pub id: u32,
+    /// * `String` - Post項目のタイトル
     pub title: String,
+    /// * `String` - Post項目の本文
     pub body: String,
 }
 
 const BASE_URL: &str = "https://jsonplaceholder.typicode.com";
 
+/// 指定されたIDを持つPost項目を取得します。
+/// # 引数
+/// * `id` - Post項目のID
+/// # 戻り値
+/// * `Result<Post, Box<dyn std::error::Error>>` - Post項目を返します。
+/// * `Box<dyn std::error::Error>` - エラーを返します。
+/// # 例外
+/// * `std::error::Error` - エラーを返します。
+/// # 詳細
+/// この関数は、指定されたIDを持つPost項目を取得します。
+/// ## 例
+/// ```rust
+/// use posts::api::get_post;
+/// async {
+///     let post = get_post(1).await.unwrap();
+///     assert_eq!(post.id, 1);
+/// };
+/// ```
+/// # 例外
+/// この関数は、エラーを返します。
+/// ## 例
+/// ```rust
+/// use posts::api::get_post;
+/// async {
+///     let post = get_post(10000).await;
+///     assert!(post.is_err());
+/// };
+/// ```
 pub async fn get_post(id: u32) -> Result<Post, Box<dyn std::error::Error>> {
     let url = format!("{}/posts/{}", BASE_URL, id);
     let response = reqwest::get(&url).await?.json::<Post>().await?;
     Ok(response)
 }
 
+/// Post項目のリストを取得します。
+/// # 戻り値
+/// * `Result<Vec<Post>, Box<dyn std::error::Error>>` - Post項目のリストを返します。
+/// * `Box<dyn std::error::Error>` - エラーを返します。
+/// # 例外
+/// * `std::error::Error` - エラーを返します。
+/// # 詳細
+/// この関数は、Post項目のリストを取得します。
+/// ## 例
+/// ```rust
+/// use posts::api::get_posts;
+/// async {
+///     let posts = get_posts().await.unwrap();
+///     assert!(!posts.is_empty());
+///     assert_eq!(posts.len(), 100);
+/// };
+/// ```
+/// # 例外
+/// この関数は、エラーを返します。
+/// ## 例
+/// ```rust
+/// use posts::api::get_posts;
+/// async {
+///     let posts = get_posts().await;
+///     assert!(posts.is_err());
+/// };
+/// ```
 pub async fn get_posts() -> Result<Vec<Post>, Box<dyn std::error::Error>> {
     let url = format!("{}/posts", BASE_URL);
     let response = reqwest::get(url).await?.json::<Vec<Post>>().await?;
--- src/todos/api.rs
+++ src/todos/api.rs
@@ -1,23 +1,83 @@
 use serde::Deserialize;
 
+/// jsonplaceholderのtodo構造
 #[derive(Debug, Deserialize)]
 
 pub struct Todo {
+    /// * `u32` - Todo項目のID
     #[serde(rename = "userId")]
     pub user_id: u32,
+    /// * `u32` - Todo項目のID
     pub id: u32,
+    /// * `String` - Todo項目のタイトル
     pub title: String,
+    /// * `bool` - Todo項目の完了状態
     pub completed: bool,
 }
 
 const BASE_URL: &str = "https://jsonplaceholder.typicode.com";
 
+/// 指定されたIDを持つTodo項目を取得します。
+/// # 引数
+/// * `id` - Todo項目のID
+/// # 戻り値
+/// * `Result<Todo, Box<dyn std::error::Error>>` - Todo項目を返します。
+/// * `Box<dyn std::error::Error>` - エラーを返します。
+/// # 例外
+/// * `std::error::Error` - エラーを返します。
+/// # 詳細
+/// この関数は、指定されたIDを持つTodo項目を取得します。
+/// ## 例
+/// ```rust
+/// use todos::api::get_todo;
+/// async {
+///     let todo = get_todo(1).await.unwrap();
+///     assert_eq!(todo.id, 1);
+/// };
+/// ```
+/// # 例外
+/// この関数は、エラーを返します。
+/// ## 例
+/// ```rust
+/// use todos::api::get_todo;
+/// async {
+///     let todo = get_todo(10000).await;
+///     assert!(todo.is_err());
+/// };
+/// ```
 pub async fn get_todo(id: u32) -> Result<Todo, Box<dyn std::error::Error>> {
     let url = format!("{}/todos/{}", BASE_URL, id);
     let response = reqwest::get(&url).await?.json::<Todo>().await?;
     Ok(response)
 }
 
+/// Todo項目のリストを取得します。
+/// # 戻り値
+/// * `Result<Vec<Todo>, Box<dyn std::error::Error>>` - Todo項目のリストを返します。
+/// * `Box<dyn std::error::Error>` - エラーを返します。
+/// # 例外
+/// * `std::error::Error` - エラーを返します。
+/// # 詳細
+/// この関数は、Todo項目のリストを取得します。
+/// ## 例
+/// ```rust
+/// use todos::api::get_todos;
+/// async {
+///     let todos = get_todos().await.unwrap();
+///     assert!(!todos.is_empty());
+///     assert_eq!(todos.len(), 200);
+/// };
+/// ```
+/// # 例外
+/// この関数は、エラーを返します。
+/// ## 例
+/// ```rust
+/// use todos::api::get_todos;
+/// async {
+///     let todos = get_todos().await;
+///     assert!(todos.is_err());
+/// };
+/// ```
 pub async fn get_todos() -> Result<Vec<Todo>, Box<dyn std::error::Error>> {
     let url = format!("{}/todos", BASE_URL);
     let response = reqwest::get(url).await?.json::<Vec<Todo>>().await?;
